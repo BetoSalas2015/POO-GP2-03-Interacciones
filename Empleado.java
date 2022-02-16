da1be4a233e90e84bc4cc5a3fa7837a98bbd1e37
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-02-15 18:46:04 -0600

    Sesion 13 - 15/02/2022

--- Empleado.java
+++ Empleado.java
@@ -28,5 +28,41 @@ public class Empleado
         salario = (nuevoSalario > 0) ? nuevoSalario : 1.0;
     }
     
+    public double getSalario() {
+        return salario;
+    }
     
+    public String getNombre() {
+        return new String(nombre);
+    }
+    
+    public String getApellido() {
+        return new String(apellido);  // Enfoque 2
+    }
+    
+    public int getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+    
+    public Date getIngreso() {
+        return new Date();
+    }
+    
+    public String toString() {
+        return "Empleado: " + nombre + " " + apellido + ", ID: " + 
+                Integer.toString(numeroEmpleado) + 
+                " Ingreso: " + ingreso.toString() + ", Salario: " + 
+                Double.toString(salario);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
